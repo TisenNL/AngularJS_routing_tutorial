93419433487945f81bd7d5c06cc4e17e661ecf9b
TIAGO NEVES LEAO COSTA LINO <tiagobrasil18@gmail.com>
2023-11-03 14:04:55 -0300

    lesson 29

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,23 +1,25 @@
 ﻿/// <reference path="angular.js" />
+/// <reference path="angular-route.js" />
 
-var app = angular.module("Demo", ["ngRoute"])
+var app = angular
+                 .module("Demo", ["ngRoute"])
                  .config(function ($routeProvider, $locationProvider) {
                      $routeProvider
-                        .when("/home", {
+                         .when("/home", {
                             templateUrl: "Templates/home.html",
                             controller: "homeController"
-                        })
-                        .when("/courses", {
+                         })
+                         .when("/courses", {
                          templateUrl: "Templates/courses.html",
                          controller: "coursesController"
-                        })
-                        .when("/students", {
+                         })
+                         .when("/students", {
                          templateUrl: "Templates/students.html",
                          controller: "studentsController"
-                        })
-                        .otherwise({
+                         })
+                         .otherwise({
                             redirectTo : "/home"
-                        })
+                         })
                      $locationProvider.html5Mode(true);
                  })
                  .controller("homeController", function ($scope) {
