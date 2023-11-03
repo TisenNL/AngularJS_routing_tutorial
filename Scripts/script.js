133a299d79052256a743e7c69064619f2aea9278
TIAGO NEVES LEAO COSTA LINO <tiagobrasil18@gmail.com>
2023-11-03 15:37:46 -0300

    lesson 32

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,44 +7,51 @@ var app = angular
                      $routeProvider
                          .when("/home", {
                              templateUrl: "Templates/home.html",
-                             controller: "homeController"
+                             controller: "homeController as homeCtrl",
+                             controllerAs: "homeCtrl"
                          })
                          .when("/courses", {
                              templateUrl: "Templates/courses.html",
-                             controller: "coursesController"
+                             controller: "coursesController",
+                             controllerAs: "coursesCtrl"
                          })
                          .when("/students", {
                              templateUrl: "Templates/students.html",
-                             controller: "studentsController"
+                             controller: "studentsController",
+                             controllerAs: "studentsCtrl"
                          })
                          .when("/students/:id", {
                              templateUrl: "Templates/studentDetails.html",
-                             controller: "studentDetailsController"
+                             controller: "studentDetailsController",
+                             controllerAs: "studentDetailsCtrl"
                          })
                          .otherwise({
                              redirectTo: "/home"
                          })
                      $locationProvider.html5Mode(true);
                  })
-                 .controller("homeController", function ($scope) {
-                     $scope.message = "Home Page";
+                 .controller("homeController", function () {
+                     this.message = "Home Page";
                  })
-                 .controller("coursesController", function ($scope) {
-                     $scope.courses = ["C#", "VB.NET", "SQL Server", "ASP.NET"];
+                 .controller("coursesController", function () {
+                     this.courses = ["C#", "VB.NET", "SQL Server", "ASP.NET"];
                  })
-                 .controller("studentsController", function ($scope, $http) {
+                 .controller("studentsController", function ($http) {
+                     var vm = this;
+
                      $http.get("StudentService.asmx/GetAllStudents")
                           .then(function (response) {
-                              $scope.students = response.data
+                              vm.students = response.data
                           })
                  })
-                .controller("studentDetailsController", function ($scope, $http, $routeParams) {
+                .controller("studentDetailsController", function ($http, $routeParams) {
+                    var vm = this;
                     $http({
                         url: "StudentService.asmx/GetStudent",
                         params: { id: $routeParams.id },
                         method: "get"
                     })
                     .then(function (response) {
-                        $scope.student = response.data
+                        vm.student = response.data
                     })
                 });
