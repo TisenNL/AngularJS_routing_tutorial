78c880f5027b03a1bb02072b6382416ee3f395f6
TIAGO NEVES LEAO COSTA LINO <tiagobrasil18@gmail.com>
2023-11-07 18:12:34 -0300

    [lesson/48] - end of lesson

--- Scripts/script.js
+++ Scripts/script.js
@@ -11,13 +11,21 @@ var app = angular
                              url: "/home",
                              templateUrl: "Templates/home.html",
                              controller: "homeController as homeCtrl",
-                             controllerAs: "homeCtrl"
+                             controllerAs: "homeCtrl",
+                             data: {
+                                 customData1: "Home State Custom Data 1",
+                                 customData2: "Home State Custom Data 2",
+                             }
                          })
                          .state("courses", {
                              url: "/courses",
                              templateUrl: "Templates/courses.html",
                              controller: "coursesController",
-                             controllerAs: "coursesCtrl"
+                             controllerAs: "coursesCtrl",
+                             data: {
+                                 customData1: "Courses State Custom Data 1",
+                                 customData2: "Courses State Custom Data 2",
+                             }
                          })
                          .state("students", {
                              url: "/students",
@@ -48,8 +56,14 @@ var app = angular
                    
                      //$locationProvider.html5Mode(true);
                  })
-                 .controller("homeController", function () {
+                 .controller("homeController", function ($state) {
                      this.message = "Home Page";
+
+                     this.homeCustomData1 = $state.current.data.customData1
+                     this.homeCustomData2 = $state.current.data.customData2
+
+                     this.coursesCustomData1 = $state.get("courses").data.customData1
+                     this.coursesCustomData2 = $state.get("courses").data.customData2
                  })
                  .controller("coursesController", function () {
                      this.courses = ["C#", "VB.NET", "SQL Server", "ASP.NET"];
